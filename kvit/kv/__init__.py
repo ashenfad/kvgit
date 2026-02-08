8573997ed53cf37584a0791ca665160bad1c891d
Adam Ashenfelter <ashenfad@gmail.com>
2026-02-08 13:37:40 -0800

    refactor: remove write-behind

--- kvit/kv/__init__.py
+++ kvit/kv/__init__.py
@@ -4,6 +4,4 @@ from .base import KVStore
 from .composite import Composite
 from .disk import Disk
 from .memory import Memory
-from .write_behind import WriteBehind
-
-__all__ = ["Composite", "Disk", "KVStore", "Memory", "WriteBehind"]
+__all__ = ["Composite", "Disk", "KVStore", "Memory"]
