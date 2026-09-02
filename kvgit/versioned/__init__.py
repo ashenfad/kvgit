17c606c5a3268620259e7b29cbf27c643a3aad3a
Adam Ashenfelter <ashenfad@gmail.com>
2026-09-02 16:34:11 -0700

    feat(tags): export TagInfo from kvgit.versioned

--- kvgit/versioned/__init__.py
+++ kvgit/versioned/__init__.py
@@ -1,12 +1,13 @@
 """Versioned store implementations."""
 
 from .kv import VersionedKV
-from .protocol import BytesMergeFn, DiffResult, MergeResult, Versioned
+from .protocol import BytesMergeFn, DiffResult, MergeResult, TagInfo, Versioned
 
 __all__ = [
     "BytesMergeFn",
     "DiffResult",
     "MergeResult",
+    "TagInfo",
     "Versioned",
     "VersionedKV",
 ]
